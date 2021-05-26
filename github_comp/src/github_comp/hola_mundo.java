ff52f60f94ade24a28b6375fc7dfa2f7c54611bb
juanm <juanm@LAPTOP-OLS0RAHG>
2021-05-27 01:31:50 +0200

    Heyyyyyy

--- github_comp/src/github_comp/hola_mundo.java
+++ github_comp/src/github_comp/hola_mundo.java
@@ -5,6 +5,7 @@ public class hola_mundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Esta es la actualización numero 1");
+		System.out.println("Y esta la segunda");
 	}
 
 }
